60e33c6ee608e3848313310a0ae183249989efd2
jiyun <sprare06@gmail.com>
2022-07-24 11:51:53 +0900

    부모에 arr 추가 - props로 전달되는 값이 참조형 자료면 메모이제이션안됨

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [Counter, setCounter] = useState(0);
   const [Input, setInput] = useState("");
 
+  const arr = [1, 2, 3];
+
   return (
     <div className="App">
       <h1>Parent: {Counter}</h1>
@@ -15,7 +17,8 @@ function App() {
         value={Input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <Child Counter={Counter} />
+      {/* 자식 컴포넌트에 prop로 전달되는 값이 원시형 다료면 재렌더링 방지 기능, 만약 참조형 자료가 전달 되면 재렌더링 방지가 안된다. */}
+      <Child Counter={Counter} arr={arr} />
     </div>
   );
 }
