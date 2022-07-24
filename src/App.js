f03465fb002c07d1fa836aba4e2349bb729d49ed
jiyun <sprare06@gmail.com>
2022-07-24 11:45:05 +0900

    부모 input 추가 - 메모이제이션 처리해놓으면 자식 컴포넌트와 상관이 없는 부모의 스테이트값 변경시에도 재랜더링 막아줌

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import Child from "./Child";
 import { useState } from "react";
 
 function App() {
-  const [Counter, setCounter] = useState(0);
   console.log("Parent");
+  const [Counter, setCounter] = useState(0);
+  const [Input, setInput] = useState("");
 
   return (
     <div className="App">
       <h1>Parent: {Counter}</h1>
       <button onClick={() => setCounter(Counter + 1)}>plus</button>
+      <input
+        type="text"
+        value={Input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <Child Counter={Counter} />
     </div>
   );
